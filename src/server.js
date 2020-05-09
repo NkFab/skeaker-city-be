e9322bb73b8395b8916c36a2e5e73606dc02aea5
nkfab <fabjeezy@gmail.com>
2020-05-09 22:17:51 +0200

    :rocket: Add CI/CD

--- src/server.js
+++ src/server.js
@@ -2,17 +2,17 @@ import "@babel/polyfill";
 import express from "express";
 import cors from "cors";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.get("/", (_req, res) => {
+const server = express();
+server.use(cors());
+server.use(express.json());
+server.get("/", (_req, res) => {
   return res.status(200).json({
     data: { message: "Welcome to Sneakers City API" }
   });
 });
-app.use("*", (_req, res) =>
+server.use("*", (_req, res) =>
   res.status(404).json({
     error: { message: "API endpoint not found!" }
   })
 );
-export default app;
+export default server;
